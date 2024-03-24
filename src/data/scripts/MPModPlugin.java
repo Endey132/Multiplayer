e9579eac970fae866158b868599c4b4d3ee617f0
EndeyDaBoss <endeydaboss132@gmail.com>
2024-03-23 19:24:55 -0700

    !

--- src/data/scripts/MPModPlugin.java
+++ src/data/scripts/MPModPlugin.java
@@ -3,10 +3,17 @@ package data.scripts;
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.PluginPick;
-import com.fs.starfarer.api.Script;
 import com.fs.starfarer.api.campaign.CampaignPlugin;
 import com.fs.starfarer.api.combat.*;
-import data.scripts.plugins.MPBasePlugin;
+import data.scripts.net.data.DataGenManager;
+import data.scripts.net.data.packables.entities.projectiles.BallisticProjectileData;
+import data.scripts.net.data.packables.entities.projectiles.MissileData;
+import data.scripts.net.data.packables.entities.projectiles.MovingRayData;
+import data.scripts.net.data.packables.entities.ships.*;
+import data.scripts.net.data.packables.metadata.*;
+import data.scripts.net.data.records.*;
+import data.scripts.net.data.records.collections.ListenArrayRecord;
+import data.scripts.net.data.records.collections.SyncingListRecord;
 import data.scripts.plugins.MPClientPlugin;
 import data.scripts.plugins.MPPlugin;
 
@@ -21,72 +28,25 @@ import data.scripts.plugins.ai.MPDefaultAutofireAIPlugin;
 import data.scripts.plugins.ai.MPDefaultMissileAIPlugin;
 
 public class MPModPlugin extends BaseModPlugin {
-
     public static String VERSION = "v0.1.1";
 
     private static MPPlugin PLUGIN;
 
     /**
-     * Bypass Starsector's class loader restrictions by loading through a custom class loader
-     * Allows use of reflection, java.io, etc. as long as the class was generated through the custom class loader, or within a class that was.
-     * Credit to andylizi (Method originates from his Planet Search plugin, found here https://github.com/andylizi/starsector-planet-search)
+     * Disables Starsector's class loader restrictions
+     * Credit to Andylizi (Method originates from his Planet Search plugin, found here https://github.com/andylizi/starsector-planet-search)
      * Alex's stance on bypassing class loader https://fractalsoftworks.com/forum/index.php?topic=23229.msg354196
      */
-    public static final ClassLoader MPClassLoader;
     static{
-        ClassLoader loader;
         try{
             ClassLoader cl = MPModPlugin.class.getClassLoader();
             while (cl != null && !(cl instanceof URLClassLoader)) cl = cl.getParent();
             if(cl == null) throw new RuntimeException("Unable to find URLClassLoader");
             URL[] urls = ((URLClassLoader)cl).getURLs();
-
-            loader = new TempClassLoader(urls, ClassLoader.getSystemClassLoader());
-        } catch (RuntimeException | Error ex){
-            throw ex;
-        } catch (Throwable t){
-            throw new ExceptionInInitializerError(t);
-        }
-        MPClassLoader = loader;
-    }
-
-    /**
-     * Map for converting between wrappers and primitive types, for use in MPLoadClass
-     */
-    private final static Map<Class<?>, Class<?>> wPMap = new HashMap<>();
-    static {
-        wPMap.put(Boolean.class, boolean.class);
-        wPMap.put(Byte.class, byte.class);
-        wPMap.put(Short.class, short.class);
-        wPMap.put(Character.class, char.class);
-        wPMap.put(Integer.class, int.class);
-        wPMap.put(Long.class, long.class);
-        wPMap.put(Float.class, float.class);
-        wPMap.put(Double.class, double.class);
-    }
-
-    /**
-     * Returns the constructor output of a specified class, through the custom class loader
-     */
-    public static <T> T MPLoadClass(String name, Object[] args){
-        try{
-            Global.getLogger(MPModPlugin.class).debug("Custom Loading class: " + name);
-
-            if(args != null){
-                ArrayList<Class<?>> argTypes = new ArrayList<>();
-                ArrayList<Object> argList = new ArrayList<>();
-                for(int i = 0; i < args.length; i++){
-                    Object arg = args[i];
-                    Class<?> argClass = arg.getClass();
-                    if(wPMap.containsKey(argClass))
-                        argClass = wPMap.get(argClass);
-                    argTypes.add(argClass);
-                    argList.add(arg);
-                }
-                return (T)MethodHandles.lookup().findConstructor(Class.forName(name, true, MPClassLoader), MethodType.methodType(void.class, argTypes)).invokeWithArguments(argList);
-            }
-
-            return (T) MethodHandles.lookup().findConstructor(Class.forName(name, true, MPClassLoader), MethodType.methodType(void.class)).invoke();
+            Class<?> cls = new TempClassLoader(urls, ClassLoader.getSystemClassLoader()).loadClass(MPModPlugin.class.getPackage().getName() + ".loading.ClassLoaderEditor");
+            // Creates instance of ClassLoaderEditor through the temporary class loader
+            MethodHandles.lookup().findConstructor(cls, MethodType.methodType(void.class)).invoke();
+            Global.getLogger(MPModPlugin.class).info("Disabled Class Loading Restrictions");
         } catch (RuntimeException | Error ex){
             throw ex;
         } catch (Throwable t){
@@ -94,23 +54,40 @@ public class MPModPlugin extends BaseModPlugin {
         }
     }
 
-    /**
-     * Initialize MPBasePlugin & ClassLoaderEditor
-     */
     @Override
     public void onApplicationLoad(){
-        Script base = MPLoadClass(MPModPlugin.class.getPackage().getName() + ".plugins.MPBasePlugin", null);
-        base.run();
-        Global.getLogger(MPModPlugin.class).info("Initialized MPBasePlugin");
+        BallisticProjectileData.TYPE_ID = DataGenManager.registerEntityType(BallisticProjectileData.class);
+        MissileData.TYPE_ID = DataGenManager.registerEntityType(MissileData.class);
+        MovingRayData.TYPE_ID = DataGenManager.registerEntityType(MovingRayData.class);
+        ShieldData.TYPE_ID = DataGenManager.registerEntityType(ShieldData.class);
+        WeaponData.TYPE_ID = DataGenManager.registerEntityType(WeaponData.class);
+        ShipData.TYPE_ID = DataGenManager.registerEntityType(ShipData.class);
+        VariantData.TYPE_ID = DataGenManager.registerEntityType(VariantData.class);
+        ChatListenData.TYPE_ID = DataGenManager.registerEntityType(ChatListenData.class);
+        ClientConnectionData.TYPE_ID = DataGenManager.registerEntityType(ClientConnectionData.class);
+        ClientData.TYPE_ID = DataGenManager.registerEntityType(ClientData.class);
+        LobbyData.TYPE_ID = DataGenManager.registerEntityType(LobbyData.class);
+        ClientPlayerData.TYPE_ID = DataGenManager.registerEntityType(ClientPlayerData.class);
+        ServerPlayerData.TYPE_ID = DataGenManager.registerEntityType(ServerPlayerData.class);
+        ServerConnectionData.TYPE_ID = DataGenManager.registerEntityType(ServerConnectionData.class);
 
-        MPLoadClass(MPModPlugin.class.getPackage().getName() + ".loading.ClassLoaderEditor", null);
-        Global.getLogger(MPBasePlugin.class).info("Initialized ClassLoaderEditor");
+        Float32Record.setTypeId(DataGenManager.registerRecordType(Float32Record.class.getSimpleName(), Float32Record.getDefault()));
+        IntRecord.setTypeId(DataGenManager.registerRecordType(IntRecord.class.getSimpleName(), IntRecord.getDefault()));
+        StringRecord.setTypeId(DataGenManager.registerRecordType(StringRecord.class.getSimpleName(), StringRecord.getDefault()));
+        Vector2f32Record.setTypeId(DataGenManager.registerRecordType(Vector2f32Record.class.getSimpleName(), Vector2f32Record.getDefault()));
+        Vector3f32Record.setTypeId(DataGenManager.registerRecordType(Vector3f32Record.class.getSimpleName(), Vector3f32Record.getDefault()));
+        SyncingListRecord.setTypeId(DataGenManager.registerRecordType(SyncingListRecord.class.getSimpleName(), new SyncingListRecord<>(new ArrayList<>(), (byte) -1)));
+        Float16Record.setTypeId(DataGenManager.registerRecordType(Float16Record.class.getSimpleName(), Float16Record.getDefault()));
+        ByteRecord.setTypeId(DataGenManager.registerRecordType(ByteRecord.class.getSimpleName(), ByteRecord.getDefault()));
+        Vector2f16Record.setTypeId(DataGenManager.registerRecordType(Vector2f16Record.class.getSimpleName(), Vector2f16Record.getDefault()));
+        ShortRecord.setTypeId(DataGenManager.registerRecordType(ShortRecord.class.getSimpleName(), ShortRecord.getDefault()));
+        ListenArrayRecord.setTypeId(DataGenManager.registerRecordType(ListenArrayRecord.class.getSimpleName(), new ListenArrayRecord<>(new ArrayList<>(), (byte) -1)));
     }
 
     @Override
     public PluginPick<AutofireAIPlugin> pickWeaponAutofireAI(WeaponAPI weapon) {
         if (getPlugin() != null && getPlugin().getType() == MPPlugin.PluginType.CLIENT) {
-            MPDefaultAutofireAIPlugin plugin = MPLoadClass(MPModPlugin.class.getPackage().getName() + ".ai.MPDefaultAutofireAIPlugin", new Object[]{weapon});
+            MPDefaultAutofireAIPlugin plugin = new MPDefaultAutofireAIPlugin(weapon);
 
             ShipAPI ship = weapon.getShip();
             MPClientPlugin clientPlugin = (MPClientPlugin) getPlugin();
@@ -127,7 +104,7 @@ public class MPModPlugin extends BaseModPlugin {
     @Override
     public PluginPick<MissileAIPlugin> pickMissileAI(MissileAPI missile, ShipAPI launchingShip) {
         if (getPlugin() != null && getPlugin().getType() == MPPlugin.PluginType.CLIENT) {
-            MPDefaultMissileAIPlugin plugin = MPLoadClass(MPModPlugin.class.getPackage().getName() + ".ai.MPDefaultMissileAIPlugin", null);
+            MPDefaultMissileAIPlugin plugin = new MPDefaultMissileAIPlugin();
 
             return new PluginPick<>((MissileAIPlugin) plugin, CampaignPlugin.PickPriority.HIGHEST);
         }
--- src/data/scripts/loading/ClassLoaderEditor.java
+++ src/data/scripts/loading/ClassLoaderEditor.java
@@ -1,40 +1,81 @@
 package data.scripts.loading;
 
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.loading.scripts.OoOO;
+import org.apache.log4j.Logger;
 import org.objectweb.asm.*;
-import org.objectweb.asm.commons.LocalVariablesSorter;
+import org.objectweb.asm.util.TraceClassVisitor;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.security.ProtectionDomain;
 
 /**
- * Edits starsector's class loader during runtime to disable loading restrictions
+ * Edits Starsector's class loader in memory to disable restrictions
  */
 public class ClassLoaderEditor implements Opcodes {
-    static String className = "com.fs.starfarer.loading.scripts.OoOO";
-    public ClassLoaderEditor() throws IOException {
+    private static final Logger LOGGER = Global.getLogger(ClassLoaderEditor.class);
+    private static final String className = "com.fs.starfarer.loading.scripts.OoOO";
+    private static final ClassLoader cl = OoOO.getSystemClassLoader();
+    public ClassLoaderEditor() throws Throwable {
+        LOGGER.info("Running constructor");
         ClassReader cr = new ClassReader(className);
         ClassWriter cw = new ClassWriter(cr, 0);
-        cr.accept(new ClassVisitor(ASM9, cw){
-            @Override
-            public MethodVisitor visitMethod(
-                    int access,
-                    String name,
-                    String desc,
-                    String signature,
-                    String[] exceptions){
-                MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-                if(name.contains("loadClass(String var1, boolean var2)")){
-                    return new EditedClassLoader(access, desc, mv);
-                }
-                return mv;
-            }
-        }, 0);
+        ClassLoaderTransform cv = new ClassLoaderTransform(cw);
+        cr.accept(cv, 0);
+        byte[] data = cw.toByteArray();
+
+        MethodHandle m = MethodHandles.lookup().findVirtual(cl.getClass(), "defineMyClass", MethodType.methodType(
+                Class.class, String.class, byte[].class, int.class, int.class, ProtectionDomain.class));
+        try{
+            m.invoke(cl, className, data, 0, data.length, ClassLoaderEditor.class.getProtectionDomain());
+        }  catch (RuntimeException | Error e){
+            throw e;
+        } catch (Throwable t) {
+            throw new AssertionError("unreachable", t);
+        }
+        ClassReader debugReader = new ClassReader(className);
+        File f = new File("grug.txt");
+        try{
+            f.createNewFile();
+        } catch(Exception e){
+
+        }
+        PrintWriter pw = null;
+        try{
+            pw = new PrintWriter(f);
+        } catch (Exception e){
+
+        }
+        TraceClassVisitor tcv = new TraceClassVisitor(pw);
+        debugReader.accept(tcv, 0);
+        LOGGER.info("Completed constructor");
     }
-    public class EditedClassLoader extends LocalVariablesSorter implements Opcodes {
-        EditedClassLoader(int access, String desc, MethodVisitor mv){
-            super(ASM9, access, desc, mv);
+    private class ClassLoaderTransform extends ClassVisitor implements Opcodes{
+        ClassLoaderTransform(ClassVisitor cv){
+            super(ASM9, cv);
+            LOGGER.info("Constructed ClassLoaderTransform");
+        }
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions){
+            LOGGER.debug("ClassLoaderEditor Visiting method name: ["+name+"], desc: ["+desc+"], signature: ["+signature+"]");
+            if(name.contains("loadClass")){
+                return new LoadClassMethodVisitor(cv.visitMethod(access, name, desc, signature, exceptions));
+            }
+            return cv.visitMethod(access, name, desc, signature, exceptions);
+        }
+    }
+    private class LoadClassMethodVisitor extends MethodVisitor implements Opcodes {
+        LoadClassMethodVisitor(MethodVisitor mv){
+            super(ASM9, mv);
+            LOGGER.info("Constructed LoadClassMethodVisitor");
         }
         @Override
         public void visitLineNumber(int line, Label start){
+            LOGGER.debug("LoadClassMethodVisitor Visiting line number: ["+line+"]");
             if(!(line >= 23 && line < 33)){
                 super.visitLineNumber(line, start);
             }
